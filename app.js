2605e2fbf1128603af6fe6af0a501aee7b50d839
mever-l <mor.lempert@gmail.com>
2024-11-19 11:22:51 -0800

    feat(init): connect to mongo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT;
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL );
+mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection
 db.on('error', (err) => console.error(err));
 db.once('open', () => console.log('Connected to Mongo :)'))
